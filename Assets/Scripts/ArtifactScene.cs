44bc9cb13e23957e11e43f29a4e0d2c8108c7e8b
NimaSattari <54527075+NimaSattari@users.noreply.github.com>
2019-10-18 11:26:54 +0330

    2 Level added

---
+++ Assets/Scripts/ArtifactScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtifactScene : MonoBehaviour
+{
+    public GameObject Exit;
+
+    private void Start()
+    {
+        Exit.GetComponent<BoxCollider2D>().enabled = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Exit.GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+}
